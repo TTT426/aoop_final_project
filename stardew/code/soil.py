6097c6edc42fda9184f57a56bf363d14e8ac4c00
chenmax1016 <chenmax1016.cs12@nycu.edu.tw>
2025-01-02 17:08:00 +0800

    add water soil

--- stardew/code/soil.py
+++ stardew/code/soil.py
@@ -1,6 +1,8 @@
 import pygame
 from settings import *
 from pytmx.util_pygame import load_pygame
+from support import *
+from random import choice
 
 class SoilTile(pygame.sprite.Sprite):
     def __init__(self, pos, surf, groups):
@@ -9,20 +11,28 @@ class SoilTile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         self.z = LAYERS['soil']
 
+class WaterTile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf, groups):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_rect(topleft = pos)
+        self.z = LAYERS['soil water']
+
 class SoilLayer:
     def __init__(self, all_sprites):
 
         #sprite groups
         self.all_sprites = all_sprites
         self.soil_sprites = pygame.sprite.Group()
+        self.water_sprites = pygame.sprite.Group()
 
         #graphics
-        self.soil_surf = pygame.image.load('../graphics/soil/o.png')
+        self.soil_surfs = import_folder_dict('../graphics/soil/')
+        self.water_surfs = import_folder('../graphics/soil_water/')
 
         self.create_soil_grid()
         self.create_hit_rects()
 
-    
     def create_soil_grid(self):
         ground = pygame.image.load('../graphics/world/ground.png')
         h_tiles = ground.get_width() // TILE_SIZE
@@ -52,14 +62,72 @@ class SoilLayer:
                     self.grid[y][x].append('X')
                     self.create_soil_tiles()
 
+    def water(self, point):
+        for soil_sprite in self.soil_sprites.sprites():
+            if soil_sprite.rect.collidepoint(point):
+                #add 'W' to the soil grid
+                x = soil_sprite.rect.x // TILE_SIZE
+                y = soil_sprite.rect.y // TILE_SIZE
+                self.grid[y][x].append('W')
+
+                #create water sprite    
+                pos = soil_sprite.rect.topleft
+                surf = choice(self.water_surfs)
+                WaterTile(pos,surf, [self.all_sprites, self.water_sprites])
+ 
+    def remove_water(self):
+
+        #destroy water sprites
+        for sprite in self.water_sprites.sprites():
+            sprite.kill()
+        #clean up grid
+        for row in self.grid:
+            for cell in row:
+                if 'W' in cell:
+                    cell.remove('W')
+    
     def create_soil_tiles(self):
         for index_row, row in enumerate(self.grid):
             for index_col, cell in enumerate(row):
                 if 'X' in cell:
-                    x = index_col*TILE_SIZE
-                    y = index_row*TILE_SIZE
+                    
+                    # tile options
+                    t = 'X' in self.grid[index_row-1][index_col]
+                    r = 'X' in self.grid[index_row][index_col+1]
+                    b = 'X' in self.grid[index_row+1][index_col]
+                    l = 'X' in self.grid[index_row][index_col-1]
+
+                    #no sides
+                    tile_type = 'o'
+
+                    # all sides
+                    if all((t, r, b, l)): tile_type = 'x'
+
+                    # horizontal tiles only
+                    if l and not any((t, r, b)): tile_type = 'r'
+                    if r and not any((t, l, b)): tile_type = 'l'
+                    if r and l and not any((t, b)): tile_type = 'lr'
+
+                    # vertical only
+                    if t and not any((r, l, b)): tile_type = 'b'
+                    if b and not any((r, l, t)): tile_type = 't'
+                    if b and t and not any((r, l)): tile_type = 'tb'
+
+                    # corners
+                    if l and b and not any((t, r)): tile_type = 'tr'
+                    if r and b and not any((t, l)): tile_type = 'tl'
+                    if l and t and not any((b, r)): tile_type = 'br'
+                    if r and t and not any((b, l)): tile_type = 'bl'
+
+                    # T shapes
+                    if all((t, b, r)) and not l: tile_type = 'tbr'
+                    if all((t, b, l)) and not r: tile_type = 'tbl'
+                    if all((l, r, t)) and not b: tile_type = 'lrb'
+                    if all((l, r, b)) and not t: tile_type = 'lrt'
+
+
                     SoilTile(
-                        pos = (x,y), 
-                        surf = self.soil_surf, 
+                        pos = (index_col*TILE_SIZE,index_row*TILE_SIZE), 
+                        surf = self.soil_surfs[tile_type], 
                         groups=[self.all_sprites, self.soil_sprites])
                     
--- stardew/code/support.py
+++ stardew/code/support.py
@@ -5,14 +5,22 @@ def import_folder(path):
     surface_list = []
 
     for folder_name, sub_folder, img_files in walk(path):
-
         img_files = sorted(img_files) # correct amimation order
-
         for img in img_files:
             full_path = path + '/' + img
             image_surface = pygame.image.load(full_path).convert_alpha()
             surface_list.append(image_surface)
-        
-
+            
     return surface_list 
+
+def import_folder_dict(path):
+    surface_dict = {}
+
+    for folder_name, sub_folder, img_files in walk(path):   
+        for img in img_files:
+            full_path = path + '/' + img
+            image_surface = pygame.image.load(full_path).convert_alpha()
+            surface_dict[img.split('.')[0]] = image_surface
+        
+    return surface_dict
 
